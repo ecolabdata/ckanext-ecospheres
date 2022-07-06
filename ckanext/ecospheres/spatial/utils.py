51cbc211dc0edf69cc323ebc28cca8a269327fca
alhyss <55992003+alhyss@users.noreply.github.com>
2022-07-06 23:13:52 +0200

    Update utils.py
    Modification de `build_dataset_dict_from_schema` pour que la langue principale puisse être spécifiée en argument.

--- ckanext/ecospheres/spatial/utils.py
+++ ckanext/ecospheres/spatial/utils.py
@@ -3,7 +3,8 @@ import ckan.plugins.toolkit as toolkit
 
 from ckanext.ecospheres.spatial.base import EcospheresDatasetDict
 
-def build_dataset_dict_from_schema(type='dataset'):
+
+def build_dataset_dict_from_schema(type='dataset', main_language=None):
     """Construit un dictionnaire de jeu de données d'après le schéma YAML.
     
     Parameters
@@ -11,6 +12,13 @@ def build_dataset_dict_from_schema(type='dataset'):
     type : str, default 'dataset'
         Le type de jeu de données. Il s'agit de la valeur de la
         propriété ``dataset_type`` du fichier YAML.
+    main_language : str, optional
+        S'il y a lieu, une langue dans laquelle seront supposées
+        être rédigées toutes les valeurs traduisibles dont la
+        langue n'est pas explicitement spécifiée. On utilisera
+        autant que possible un code ISO 639 sur deux caractères,
+        et plus généralement le code approprié pour désigner la
+        langue en RDF.
 
     Returns
     -------
@@ -21,8 +29,6 @@ def build_dataset_dict_from_schema(type='dataset'):
 
     """
     dataset_schema = toolkit.get_action('scheming_dataset_schema_show')(None, {'type': type})
-    return EcospheresDatasetDict(dataset_schema)
-
-
+    return EcospheresDatasetDict(dataset_schema, main_language=main_language)
 
 
