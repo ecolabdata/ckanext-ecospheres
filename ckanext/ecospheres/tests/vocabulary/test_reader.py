ce052abf8eed93e821f3e5cbaaa5d6fdfd55b1ca
alhyss <55992003+alhyss@users.noreply.github.com>
2022-09-13 18:06:19 +0200

    Update test_reader.py
    Réécriture des tests.

--- ckanext/ecospheres/tests/vocabulary/test_reader.py
+++ ckanext/ecospheres/tests/vocabulary/test_reader.py
@@ -1,40 +1,60 @@
 from builtins import object
 
-from rdflib import URIRef, Literal
-
-from ckanext.ecospheres.vocabulary import reader
+from ckanext.ecospheres.vocabulary.reader import VocabularyReader
 
 class TestIsKnownURI(object):
     
-    def test_find_recorded_vocabulary_item_from_uriref_uri(self):
-        """Vérifie que is_known_uri détecte bien comme telles les URI référencées (avec URI de type URIRef)."""
-        assert reader.is_known_uri(
-            'eu_language', 
-            URIRef('http://publications.europa.eu/resource/authority/language/FRA')
-        )
-    
-    def test_find_recorded_vocabulary_item_from_str_uri(self):
+    def test_find_recorded_vocabulary_item(self):
         """Vérifie que is_known_uri détecte bien comme telles les URI référencées (avec URI de type str)."""
-        assert reader.is_known_uri(
+        assert VocabularyReader.is_known_uri(
             'eu_language', 
             'http://publications.europa.eu/resource/authority/language/FRA'
         )
     
-    def test_dont_find_unrecorded_vocabulary_item_from_uriref_uri(self):
-        """Vérifie que is_known_uri détecte bien comme inconnues les URI non référencées (avec URI de type URIRef)."""
-        assert reader.is_known_uri(
-            'eu_language', 
-            URIRef('http://THING')
-        )
-    
-    def test_find_recorded_vocabulary_item_from_str_uri(self):
+    def test_dont_find_unrecorded_vocabulary_item(self):
         """Vérifie que is_known_uri détecte bien comme inconnues les URI non référencées (avec URI de type str)."""
-        assert reader.is_known_uri(
+        assert not VocabularyReader.is_known_uri(
             'eu_language', 
             'http://THING'
         )
 
-class TestGetURIFromLabel(object):
+class TestGetURI(object):
 
-    def test_get_uri_from_literal_skos_preflabel(self):
-        """Vérifie qu'il est possible de trouver un item à partir d'un label de type Literal."""
+    def test_get_uri_from_label(self):
+        """Vérifie qu'il est possible de trouver un item à partir de son label."""
+        assert VocabularyReader.get_uri_from_label(
+            'ecospheres_theme',
+            "Réseaux d'énergie et de communication"
+        ) == 'http://registre.data.developpement-durable.gouv.fr/ecospheres/themes-ecospheres/reseaux-d-energie-et-de-communication'
+
+    def test_get_uri_from_label_with_known_lang_tag(self):
+        """Vérifie qu'il est possible de trouver un item à partir de son label, en spécifiant une langue connue."""
+        assert VocabularyReader.get_uri_from_label(
+            'ecospheres_theme',
+            "Réseaux d'énergie et de communication",
+            language='fr'
+        ) == 'http://registre.data.developpement-durable.gouv.fr/ecospheres/themes-ecospheres/reseaux-d-energie-et-de-communication'
+
+    def test_get_uri_from_label_with_unknown_lang_tag(self):
+        """Vérifie que l'URI ne sort pas lorsque le label correspond mais pas la langue."""
+        assert VocabularyReader.get_uri_from_label(
+            'ecospheres_theme',
+            "Réseaux d'énergie et de communication",
+            language='en'
+        ) is None
+
+    def test_get_uri_from_altlabel(self):
+        """Vérifie qu'il est possible de trouver un item à partir d'un label alternatif."""
+        assert VocabularyReader.get_uri_from_label(
+            'ecospheres_theme',
+            'Habitat Politique de la ville/Construction'
+        ) == 'http://registre.data.developpement-durable.gouv.fr/ecospheres/themes-ecospheres/construction'
+
+    def test_get_uri_from_regexp(self):
+        """Vérifie qu'il est possible de trouver un item à partir d'une expression régulière."""
+        assert VocabularyReader.get_uri_from_regexp(
+            'ecospheres_theme',
+            ('VERCORS', 'Parc Naturel Régional du Vercors')
+        ) == 'http://registre.data.developpement-durable.gouv.fr/ecospheres/themes-ecospheres/zonages-d-amenagement'
+
+
