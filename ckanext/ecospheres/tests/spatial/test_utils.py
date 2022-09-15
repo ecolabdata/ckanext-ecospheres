26cfaf9276af82ca360adf0036fd247fd2d3bf18
alhyss <55992003+alhyss@users.noreply.github.com>
2022-09-15 17:49:50 +0200

    Update test_utils.py
    Ajout de tests pour  `is_valid_url`, `build_catalog_page_url` et `build_attributes_page_url`.

--- ckanext/ecospheres/tests/spatial/test_utils.py
+++ ckanext/ecospheres/tests/spatial/test_utils.py
@@ -2,7 +2,10 @@ from builtins import object
 
 from ckanext.ecospheres.spatial.utils import (
     bbox_wkt_from_coordinates, 
-    bbox_geojson_from_coordinates
+    bbox_geojson_from_coordinates,
+    build_attributes_page_url,
+    build_catalog_page_url,
+    is_valid_url
 )
 
 class TestCoordinatesOperations(object):
@@ -16,4 +19,28 @@ class TestCoordinatesOperations(object):
         assert bbox_geojson_from_coordinates(
             2.223960161, 2.46962285, 48.815608978, 48.90165329
         ) == '{"type": "Polygon", "coordinates": [[[2.223960161, 48.90165329], [2.223960161, 48.815608978], [2.46962285, 48.815608978], [2.46962285, 48.90165329], [2.223960161, 48.90165329]]]}'
+
+class TestURLOperations(object):
+
+    def test_valid_url_is_valid(self):
+        assert is_valid_url(
+            'https://www.wikipedia.org/'
+        )
+
+    def test_unvalid_url_is_not_valid(self):
+        assert not is_valid_url('truc')
+        assert not is_valid_url('https://truc')
+
+    def test_build_geoide_attributes_page_url(self):
+        assert build_attributes_page_url(
+            'http://catalogue.geo-ide.developpement-durable.gouv.fr/catalogue/srv/fre/catalog.search#/metadata',
+            'fr-120066022-jdd-192de12f-ee82-4abc-a2c0-42e30e2242a0'
+        ) == 'http://catalogue.geo-ide.developpement-durable.gouv.fr/catalogue/srv/fre/catalog.search#/metadata/fr-120066022-ca-jdd-192de12f-ee82-4abc-a2c0-42e30e2242a0'
+
+    def test_build_catalog_page_url(self):
+        assert build_catalog_page_url(
+            'http://catalogue.geo-ide.developpement-durable.gouv.fr/catalogue/srv/fre/catalog.search#/metadata',
+            'fr-120066022-jdd-192de12f-ee82-4abc-a2c0-42e30e2242a0'
+        ) == 'http://catalogue.geo-ide.developpement-durable.gouv.fr/catalogue/srv/fre/catalog.search#/metadata/fr-120066022-jdd-192de12f-ee82-4abc-a2c0-42e30e2242a0'
+
 
