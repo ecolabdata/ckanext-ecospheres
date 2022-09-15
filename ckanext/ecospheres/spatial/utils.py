8f2f5e6f2336f33467087e3ae97c0d86600207fd
alhyss <55992003+alhyss@users.noreply.github.com>
2022-09-15 17:49:36 +0200

    Update utils.py
    Ajout des fonctions `is_valid_url`, `build_catalog_page_url` et `build_attributes_page_url`.

--- ckanext/ecospheres/spatial/utils.py
+++ ckanext/ecospheres/spatial/utils.py
@@ -1,5 +1,5 @@
 
-import json
+import json, requests
 
 import ckan.plugins.toolkit as toolkit
 
@@ -100,4 +100,108 @@ def bbox_wkt_from_coordinates(west, east, south, north):
         f'{east} {north},'
         f'{west} {north}))'
     )
+
+def is_valid_url(url):
+    """Send a get request to the URL and check the response.
+
+    Parameters
+    ----------
+    url : str
+        Some URL.
+    
+    Returns
+    -------
+    bool
+        ``False`` if the status code of the response
+        indicates that some error occurred, else ``True``.
+
+    """
+    try:
+        response = requests.get(url)
+        return response.status_code == requests.codes.ok
+    except:
+        return False
+
+def build_catalog_page_url(
+    catalog_base_url, ids
+):
+    """Try to generate an URL for the dataset page on source catalog.
+
+    Parameters
+    ----------
+    catalog_base_url : str
+        Base URL for the catalog pages.
+    ids : str or list(str) or tuple(str)
+        One or more identifiers for the dataset or
+        the catalog record.
+
+    Returns
+    -------
+    str or None
+        A valid URL that should be the URL of the 
+        dataset page. Generated URLs are tested and will
+        only be returned if the GET request didn't fail.
+
+    """
+    if isinstance(ids, str):
+        ids = [ids]
+    if not ids or not catalog_base_url:
+        return
+    if not any(
+        catalog_base_url.endswith(suffix) for suffix in ('/', '#', '=')
+    ):
+        catalog_base_url = catalog_base_url + '/'
+    
+    for id in ids:        
+        url = catalog_base_url + id
+        if is_valid_url(url):
+            return url
+
+def build_attributes_page_url(
+    attributes_base_url, ids
+):
+    """Try to generate an URL for the attributes page.
+
+    Parameters
+    ----------
+    attributes_base_url : str
+        Base URL for every attributes pages of the
+        catalog.
+    ids : str or list(str) or tuple(str)
+        One or more identifiers for the dataset or
+        the catalog record.
+    
+    Returns
+    -------
+    str or None
+        A valid URL that should be the URL of the page
+        describing the attributes of the dataset.
+        Generated URLs are tested and will only be returned
+        if the GET request didn't fail.
+
+    """
+    if isinstance(ids, str):
+        ids = [ids]
+    if not ids or not attributes_base_url:
+        return
+    if not any(
+        attributes_base_url.endswith(suffix) for suffix in ('/', '#', '=')
+    ):
+        attributes_base_url = attributes_base_url + '/'
+    
+    for id in ids:
+        
+        if '-jdd-' in id:
+            # this is meant for Géo-IDE
+            url = attributes_base_url + id.replace('-jdd-', '-ca-jdd-')
+            if is_valid_url(url):
+                return url
+            else:
+                return
+        
+        url = attributes_base_url + id
+        if is_valid_url(url):
+            return url
+
+    
 
