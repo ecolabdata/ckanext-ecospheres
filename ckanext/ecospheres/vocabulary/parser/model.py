b62f545994cd147b5f19bb13c4efba3d384d17c6
alhyss <55992003+alhyss@users.noreply.github.com>
2022-12-19 16:14:51 +0100

    Update ckanext/ecospheres/vocabulary/parser/model.py
    Suppression de modifications sans objet.

--- ckanext/ecospheres/vocabulary/parser/model.py
+++ ckanext/ecospheres/vocabulary/parser/model.py
@@ -1325,8 +1325,7 @@ class VocabularyDataCluster(dict):
                 columns=['uri'],
                 refcolumns=[f'{self.label.name}.uri'],
                 ondelete='CASCADE',
-                onupdate='CASCADE',
-                                initially=True
+                onupdate='CASCADE'
 
             )
         )
