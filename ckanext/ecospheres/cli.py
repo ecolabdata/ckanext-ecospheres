2d3a465e992c808241ede3e9f17c6f08c21fbfd2
alhyss <55992003+alhyss@users.noreply.github.com>
2023-02-08 18:54:28 +0100

    Update cli.py
    Ajout de commandes pour rechercher un terme de vocabulaire d'après son label (lsearch) ou ses expressions régulières (rsearch). Amélioration de la documentation des commandes existantes.

--- ckanext/ecospheres/cli.py
+++ ckanext/ecospheres/cli.py
@@ -11,7 +11,10 @@ def vocabulary():
 def get_commands():
     return [vocabulary]
 
-@vocabulary.command()
+@vocabulary.command(
+    help=u'List the names of all vocabularies available in the CKAN database.',
+    short_help=u'List vocabularies.'
+)
 def list():
     '''List all known vocabularies, if any.
 
@@ -20,7 +23,85 @@ def list():
     if vocabularies:
         click.secho('\n'.join(vocabularies), fg=u'green')
 
-@vocabulary.command()
+@vocabulary.command(
+    help=u'Return the URI of a vocabulary item with matching label.',
+    short_help=u'Label search.'
+)
+@click.argument(u'vocabulary')
+@click.argument(u'label')
+@click.argument(u'language', required=False)
+@click.option(
+    u'-s', u'--case-sensitive', u'case_sensitive',
+    help=u'Use this flag for a case sensitive search.',
+    is_flag=True, flag_value=True
+)
+@click.option(
+    u'-a', u'--use-altlabel', u'use_altlabel',
+    help=u'Use this flag to include alternative labels.',
+    is_flag=True, flag_value=True
+)
+def lsearch(vocabulary, label, language, case_sensitive, use_altlabel):
+    '''Return the URI of a vocabulary item with matching label.
+
+    Basic usage:
+
+        >>> ckan -c ckan.ini vocabulary lsearch ecospheres_theme "Infrastructure portuaire"
+
+    Parameters
+    ----------
+    vocabulary : str
+        Name of the vocabulary, ie its ``name``
+        property in ``vocabularies.yaml``.
+    label : str
+        Some label to look up.
+    case_sensitive : bool, default False
+        If ``True``, the case will be considered when
+        testing the labels.
+    use_altlabel : bool, default False
+        Should the method search for a matching alternative
+        label if there's no match with the preferred labels?
+    
+    '''
+    uri = VocabularyReader.get_uri_from_label(
+        vocabulary=vocabulary,
+        label=label,
+        language=language,
+        case_sensitive=case_sensitive,
+        use_altlabel=use_altlabel
+    )
+    click.secho(uri or '< No result >', fg=u'green')
+
+@vocabulary.command(
+    help=u'List the URIs of all vocabulary items whose regular expression matches any of the given terms.',
+    short_help=u'Regular expression search.'
+)
+@click.argument(u'vocabulary')
+@click.argument(u'terms', nargs=-1)
+def rsearch(vocabulary, terms):
+    '''List URIs of all vocabulary items whose regular expression matches any of the given terms.
+    
+    Parameters
+    ----------
+    vocabulary : str
+        Name of the vocabulary, ie its ``name``
+        property in ``vocabularies.yaml``.
+    terms : str or list(str) or tuple(str)
+        Metadata values to test against the regular
+        expressions associated with the concepts.
+    
+    '''
+    uris = VocabularyReader.get_uris_from_regexp(
+        vocabulary=vocabulary, terms=terms
+    )
+    if uris:
+        click.secho('\n'.join(uris), fg=u'green')
+    else:
+        click.secho('< No result >', fg=u'green')
+
+@vocabulary.command(
+    help=u'Load vocabularies into CKAN database.',
+    short_help=u'Load vocabularies.'
+)
 @click.argument(u'name', required=False, nargs=-1)
 @click.option(u'-e', u'--exclude', u'exclude', multiple=True, help=u'A vocabulary not to load.')
 def load(name, exclude):
