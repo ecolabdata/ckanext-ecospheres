f8d1a0cc3c6b20486bfd042098eb5e4b6dc7088c
alhyss <55992003+alhyss@users.noreply.github.com>
2023-03-30 20:52:03 +0200

    Update __init__.py
    Première phase de la refonte de l'exposition DCAT, avec la réécriture de la méthode `graph_from_dataset`.
    
    La classe `FranchDCATAPProfile` devient `EcospheresDCATAPProfile`.

--- ckanext/ecospheres/dcat/profiles/__init__.py
+++ ckanext/ecospheres/dcat/profiles/__init__.py
@@ -1,16 +1,279 @@
+
+import logging
+
+from rdflib import Literal, BNode
+from rdflib.util import from_n3
+from rdflib.namespace import Namespace
+
+import ckan.plugins.toolkit as toolkit
+from ckanext.dcat.profiles import RDFProfile, CleanedURIRef
+
 from .dataset.parse_dataset import parse_dataset as _parse_dataset
-from .graph.graph_from_dataset import graph_from_dataset as _graph_from_dataset
 from .graph.graph_from_catalog import graph_from_catalog as _graph_from_catalog
 
-from ckanext.dcat.profiles import (
-    RDFProfile,
+logger = logging.getLogger(__name__)
+
+ADMS = Namespace('http://www.w3.org/ns/adms#')
+CNT = Namespace('http://www.w3.org/2011/content#')
+DCAT = Namespace('http://www.w3.org/ns/dcat#')
+DCATAP = Namespace('http://data.europa.eu/r5r/')
+DCT = Namespace('http://purl.org/dc/terms/')
+DCTYPE = Namespace('http://purl.org/dc/dcmitype/')
+DQV = Namespace('http://www.w3.org/ns/dqv#')
+FOAF = Namespace('http://xmlns.com/foaf/0.1/')
+GEODCAT = Namespace('http://data.europa.eu/930/')
+GSP = Namespace('http://www.opengis.net/ont/geosparql#')
+LOCN = Namespace('http://www.w3.org/ns/locn#')
+ORG = Namespace('http://www.w3.org/ns/org#')
+OWL = Namespace('http://www.w3.org/2002/07/owl#')
+PROV = Namespace('http://www.w3.org/ns/prov#')
+RDF = Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#')
+RDFS = Namespace('http://www.w3.org/2000/01/rdf-schema#')
+SDO = Namespace('http://schema.org/')
+SH = Namespace('http://www.w3.org/ns/shacl#')
+SKOS = Namespace('http://www.w3.org/2004/02/skos/core#')
+VCARD = Namespace('http://www.w3.org/2006/vcard/ns#')
+XSD = Namespace('http://www.w3.org/2001/XMLSchema#')
+
+NAMESPACES = {
+    'adms': ADMS,
+    'cnt': CNT,
+    'dcat': DCAT,
+    'dcatap': DCATAP,
+    'dct': DCT,
+    'dctype': DCTYPE,
+    'dqv': DQV,
+    'foaf': FOAF,
+    'geodcat': GEODCAT,
+    'gsp': GSP,
+    'locn': LOCN,
+    'org': ORG,
+    'owl': OWL,
+    'prov': PROV,
+    'rdf': RDF,
+    'rdfs': RDFS,
+    'sdo': SDO,
+    'sh': SH,
+    'skos': SKOS,
+    'vcard': VCARD,
+    'xsd': XSD
+    }
+
+def dataset_schema():
+    '''Return the schema for datasets' metadata.'''
+    return toolkit.get_action('scheming_dataset_schema_show')(
+        None, {'type': 'dataset'}
     )
-class FranchDCATAPProfile(RDFProfile):
+
+def clean_empty_data(data):
+    '''Recursively delete all keys without meaningful value.'''
+    if isinstance(data, str):
+        return not bool(data)
+    elif isinstance(data, dict):
+        to_delete = []
+        for key, value in data.copy().items():
+            if clean_empty_data(value):
+                to_delete.append(key) 
+        for key in to_delete:
+            del data[key]
+        return not bool(data)
+    elif isinstance(data, list):
+        to_delete = []
+        for elem in data:
+            if clean_empty_data(elem):
+                to_delete.append(elem)
+        for elem in to_delete:
+            data.remove(elem)
+        return not bool(data)
+    else:
+        return data is None
+
+class EcospheresDCATAPProfile(RDFProfile):
+
     def parse_dataset(self, dataset_dict, dataset_ref):
         return _parse_dataset(self, dataset_dict, dataset_ref)
- 
-    def graph_from_dataset(self, dataset_dict, dataset_ref):
-        return _graph_from_dataset(self, dataset_dict, dataset_ref)
-
+        # TODO: à réécrire [LL-2023-03-03]
+    
     def graph_from_catalog(self, catalog_dict, catalog_ref):
         return  _graph_from_catalog(self, catalog_dict, catalog_ref)
+        # TODO: à réécrire [LL-2023-03-03]
+
+    def graph_from_dataset(self, dataset_dict, dataset_ref):
+
+        dataset_dict = dataset_dict.copy()
+        if clean_empty_data(dataset_dict):
+            return
+
+        for prefix, namespace in NAMESPACES.items():
+            self.g.namespace_manager.bind(
+                prefix, namespace, override=True, replace=True
+            )
+        
+        schema = dataset_schema()
+        if dataset_ref_str := dataset_dict.get('uri'):
+            dataset_ref = CleanedURIRef(dataset_ref_str)
+        else:
+            logger.debug('< {dataset_ref} > Skipped dataset without URI')
+            return
+        # for s, p, o in self.g:
+        #     if s == dataset_ref:
+        #         self.g.remove((s, p, o))
+        self.g.add((dataset_ref, RDF.type, DCAT.Dataset))
+
+        # champs décrivant le jeu de données
+        self._graph_from_dataset_and_schema(
+            dataset_dict,
+            schema.get('dataset_fields'),
+            dataset_ref,
+            dataset_ref
+        )
+
+        # champs décrivant les ressources
+        resources = dataset_dict.get('resources')
+        if resources:
+            for resource_dict in resources:
+                node = BNode()
+                self.g.add((dataset_ref, DCAT.distribution, node))
+                self.g.add((node, RDF.type, DCAT.Distribution))
+                self._graph_from_dataset_and_schema(
+                    resource_dict,
+                    schema.get('resource_fields'),
+                    node,
+                    dataset_ref
+                )
+    
+    def _read_rdf_path(self, rdf_path, subject):
+        if len(rdf_path) == 2:
+            return (
+                subject,
+                from_n3(rdf_path[0], nsm=self.g.namespace_manager),
+                from_n3(rdf_path[1], nsm=self.g.namespace_manager)
+            )
+        else:
+            node = BNode()
+            property = from_n3(rdf_path[0], nsm=self.g.namespace_manager)
+            self.g.add((subject, property, node))
+            rdftype = from_n3(rdf_path[1], nsm=self.g.namespace_manager)
+            self.g.add((node, RDF.type, rdftype))
+            return self._read_rdf_path(rdf_path[2:], node)
+
+    def _graph_from_dataset_and_schema(
+        self, fields_data, fields_schema, subject, dataset_ref
+    ):
+        if not fields_data:
+            logger.debug(
+                f'< {dataset_ref} > Missing data for < {subject} >'
+            )
+            return
+        if not fields_schema:
+            logger.debug(
+                f'< {dataset_ref} > Missing schema fragment for < {subject} >'
+            )
+            return
+        if not isinstance(fields_data, dict):
+            logger.debug(
+                f'< {dataset_ref} > Ill-formed data '
+                'skipped during serialization'
+                )
+            return
+        if not isinstance(fields_schema, list):
+            logger.debug(
+                f'< {dataset_ref} > Data for ill-formed '
+                'schema skipped during serialization'
+                )
+            return
+
+        for field, value in fields_data.items():
+
+            if (
+                field in ('uri', 'resources')
+                or value in (None, '', [], {})
+            ):
+                continue
+
+            # recherche de la description du champ
+            for field_dict in fields_schema:
+                if field_dict.get('field_name') == field:
+                    field_schema = field_dict
+                    break
+            else:
+                logger.debug(
+                    f'< {dataset_ref} > Unknown field "{field}" '
+                    'skipped during serialization'
+                )
+                continue
+            
+            value_type = field_schema.get('value_type')
+
+            rdf_path = field_schema.get('rdf_path')
+            # création des parents implicites
+            if not rdf_path:
+                continue
+            subject, property, rdftype = self._read_rdf_path(
+                rdf_path, subject
+            )
+
+            # noeud anonyme et noeud anonyme/URI
+            if (
+                value_type in ('node or uri', 'node')
+                and isinstance(value, list)
+                and value
+                and isinstance(value[0], dict)
+            ):
+                subfields_schema = field_schema.get('repeating_subfields')
+                if not subfields_schema:
+                    continue
+                for subfields_data in value:
+                    node = BNode()
+                    for subkey, subvalue in subfields_data.items():
+                        if subkey == 'uri' and subvalue:
+                            node = CleanedURIRef(subvalue)
+                    self.g.add((subject, property, node))
+                    self.g.add((node, RDF.type, rdftype))
+                    self._graph_from_dataset_and_schema(
+                        subfields_data, subfields_schema, node, dataset_ref
+                    )
+
+            # URI
+            elif value_type == 'uri':
+                if isinstance(value, str):
+                    self.g.add((subject, property, CleanedURIRef(value)))
+                elif isinstance(value, list):
+                    for v in value:
+                        if isinstance(v, str):
+                            self.g.add((subject, property, CleanedURIRef(v)))
+                        elif isinstance(v, dict):
+                            uri = v.get('uri')
+                            if uri and isinstance(uri, str):
+                                self.g.add((subject, property, CleanedURIRef(uri)))
+            
+            # valeur littérale
+            elif value_type == 'literal':
+                if (
+                    field_schema.get('translatable_values')
+                    and isinstance(value, dict)
+                ):
+                    for language, v in value.items():
+                        if isinstance(v, list):
+                            for e in v:
+                                self.g.add((subject, property, Literal(e, lang=language)))
+                        elif v:
+                            self.g.add((subject, property, Literal(v, lang=language)))
+                if isinstance(value, list):
+                    for v in value:
+                        lit = Literal(v, datatype=rdftype)
+                        # # RDFLib 6.3.2
+                        # if lit.ill_typed:
+                        #     continue
+                        self.g.add((subject, property, lit))
+                else:
+                    lit = Literal(value, datatype=rdftype)
+                    # # RDFLib 6.3.2
+                    # if lit.ill_typed:
+                    #     continue
+                    self.g.add((subject, property, lit))
+
+
+
+
+
