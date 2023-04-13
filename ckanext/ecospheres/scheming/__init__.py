2d7820ad6815872a33d59ee0e0d7bb89b4da72b0
alhyss <55992003+alhyss@users.noreply.github.com>
2023-04-13 19:29:06 +0200

    Update __init__.py
    Gestion de la récupération du schéma sous une forme mieux intégrée à CKAN.

--- ckanext/ecospheres/scheming/__init__.py
+++ ckanext/ecospheres/scheming/__init__.py
@@ -1,19 +1,22 @@
 
-# import ckan.plugins.toolkit as toolkit
-import yaml
-from pathlib import Path
+import  ckan.plugins.toolkit as toolkit
 
-from ckanext.ecospheres.scheming import __path__ as scheming_path
+class EcospheresAccessor(type):
+    def __getattr__(self, name):
+        schema = toolkit.get_action('scheming_dataset_schema_show')(
+            None, {'type': 'dataset'}
+        )
+        setattr(self, name, schema)
+        return schema
 
-def dataset_schema():
-    '''Return the schema for datasets' metadata.'''
-    # return toolkit.get_action('scheming_dataset_schema_show')(
-    #     None, {'type': 'dataset'}
-    # )
-    yaml_path = Path(scheming_path[0]) / 'ecospheres_dataset_schema.yaml'
-    if not yaml_path.exists() or not yaml_path.is_file():
-        raise FileNotFoundError('missing ecospheres_dataset_schema.yaml file')
-    with open(yaml_path, encoding='utf-8') as src:
-        return yaml.load(src, yaml.Loader)
+class EcospheresSchemas(metaclass=EcospheresAccessor):
+    '''Access to cached metadata schemas.
+    
+    The schema for a given type can be accessed by 
+    calling the class attribute named after this 
+    type.
 
-DATASET_SCHEMA = dataset_schema()
+    For datasets:
+        >>> EcospheresSchemas.dataset
+    
+    '''
